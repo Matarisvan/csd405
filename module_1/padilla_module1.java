2a5665beb76d92baa0b47d638b626ecda2f15fbc
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-01-06 15:00:53 -0500

    init_assignment

---
+++ module_1/padilla_module1.java
@@ -0,0 +1,26 @@
+/*
+ * Write a program with a class titled Fan. This class is to contain:
+
+1.  Four constants named STOPPED, SLOW, MEDIUM, and FAST. The constants are to hold the values of 0, 1, 2, and 3 respectively.
+2.  A private field named speed that holds one of the constant values with the default being STOPPED.
+3.  A private Boolean field titled on that specifies whether the fan is on or off.
+4.  A private field named radius that holds the radius of the fan with a default value of 6.
+5.  A String field that holds the color, with the default being white.
+6.  Setter and getter methods for all mutable fields.
+7.  A no-argument constructor that sets all fields with a default value.
+8.  A constructor taking arguments and setting values.
+9.  Write a toString() method that returns a description of the Fans state.
+10. Write test code that creates two instances of the Fan class, one using the default constructor and the other using the argument constructor.
+    Write code that displays the functionality of the Fan class methods.
+ */
+
+
+public class padilla_module1 {
+    public static void main(String [] args) {
+
+        // Welcome Message
+        System.out.println("\nWelcome to Module 1 for CSD405\n");
+
+    }
+    
+}
