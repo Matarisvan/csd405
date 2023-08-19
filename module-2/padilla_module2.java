b4f51017aba2933b326c9f8b51439014b6c09e37
Matarisvan <111477441+Matarisvan@users.noreply.github.com>
2023-08-19 01:03:56 -0400

    init_assignment

---
+++ module-2/padilla_module2.java
@@ -0,0 +1,56 @@
+/*
+Luis Padilla
+8/7/23
+CSD340 - Assignment 1 
+
+* Write a program with a class titled Fan. This class is to contain:
+
+1.  Four constants named STOPPED, SLOW, MEDIUM, and FAST. The constants are to hold the values of 0, 1, 2, and 3 respectively.
+2.  A private field named speed that holds one of the constant values with the default being STOPPED.
+3.  A private Boolean field titled on that specifies whether the fan is on or off.
+4.  A private field named radius that holds the radius of the fan with a default value of 6.
+5.  A String field that holds the color, with the default being white.
+6.  Setter and getter methods for all mutable fields.
+7.  A no-argument constructor that sets all fields with a default value.
+8.  A constructor taking arguments and setting values.
+9.  Write a toString() method that returns a description of the Fans state.
+10. Write test code that creates two instances of the Fan class, one using the default constructor and the other using the argument constructor.
+    Write code that displays the functionality of the Fan class methods.
+
+
+Starting with your code from Module 1 create the following new class titled UseFans:
+11. Create a collection of Fan instances.
+12. Create a method taking a collection of Fan instances for displaying without using the toString() method.
+13. Create a method that takes a single instance of a Fan for displaying without using the toString() method.
+14. Use the this reference throughout the Fan class where allowed.
+15. fnaWrite test code that creates a collection of Fans and displays the functionality of all of the Fan instances in the collection.
+ */
+
+
+public class padilla_module2 {
+    
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("\nWelcome to Module 1 for CSD405\n");
+
+        //test code
+        Fan fanDefault = new Fan();
+        System.out.println(fanDefault.getSpeed());
+        System.out.println(fanDefault.getState());
+        System.out.println(fanDefault.getRadius());
+        System.out.println(fanDefault.getColor());
+        System.out.println(fanDefault.fanDesc() + "\n");
+        
+        Fan fanArgument = new Fan(Fan.FAST, true, 21, "red");
+        System.out.println(fanArgument.getSpeed());
+        System.out.println(fanArgument.getState());
+        System.out.println(fanArgument.getRadius());
+        System.out.println(fanArgument.getColor());
+        System.out.println(fanArgument.fanDesc());
+        
+        //closing message
+        System.out.println("\ngracias\n");
+    }
+    
+}
